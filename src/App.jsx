980779da45270b63a3b6460c7802db6cb457fa0f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-20 17:13:49 -0300

    Global Styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input placeholder="Number of guests" />
+        <Input placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
