8292ba9747cbe79caa3584d5d9c4bec2d27f777f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-12 17:50:24 -0300

    Authorization: Protecting Routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,9 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
@@ -13,6 +14,7 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
     onError: (err) => {
