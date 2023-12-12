d731a224ece724eb0ea548789a90f699141b3b6e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-12 15:44:01 -0300

    Authentication: User Login with Supabase

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -9,17 +9,25 @@ const Heading = styled.h1`
     `}
 
   ${(props) =>
-    props.type === "h2" &&
+    props.as === "h2" &&
     css`
       font-size: 2rem;
       font-weight: 600;
     `}
 
     ${(props) =>
-    props.type === "h3" &&
+    props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
+    `}
+
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
     `}
 
   line-height: 1.4;
